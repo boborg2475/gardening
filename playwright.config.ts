d855be1cced8eb87aecfbdcae3c8f4e9697e01d9
Bob Katulka <bobby.katulka@gmail.com>
2026-04-04 16:41:45 -0400

    Add Playwright and lint report comments to PR via CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
 	testDir: 'tests/e2e',
-	testMatch: '**/*.e2e.{ts,js}'
+	testMatch: '**/*.e2e.{ts,js}',
+	reporter: process.env.CI
+		? [['json', { outputFile: 'reports/playwright/results.json' }], ['list']]
+		: 'list'
 });
